2e6046e3bc35e7903e5e7d128b30b50de162615c
Gustavo Morini <gustavommh@gmail.com>
2020-02-22 12:31:06 -0300

    header, logo, cart

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,3 +1,45 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import { Link } from 'react-router-dom';
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  margin: 50px 0;
+`;
+
+export const Cart = styled(Link)`
+  display: flex;
+  align-items: center;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  svg {
+    font-size: 36px;
+    color: #fff;
+  }
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #fff;
+    }
+
+    span {
+      font-size: 12px;
+      color: #999;
+    }
+  }
+`;
+
+/*
+  para o styled conseguir estilizar um componente que não é nativo do html
+  basta passalo como uma função ex: styled(Link)
+*/
