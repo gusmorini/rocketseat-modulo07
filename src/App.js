41976b477ec4877cac4199039850cc77944d6257
Gustavo Morini <gustavommh@gmail.com>
2020-02-24 10:46:55 -0300

    config reactotron

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+// configuração reactotron
+// deve vir antes da importação do store
+import './config/ReactotronConfig';
+
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
